122474c3f355b3b04380416cd98227b5a4b3b6fe
ELIAS <ELIAS@LAP-DELL>
2023-07-09 16:46:17 -0500

    Comentario en APP

--- ecarrilloTS5/ecarrilloTS5/App.xaml.cs
+++ ecarrilloTS5/ecarrilloTS5/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ecarrilloTS5
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage( new MainPage());
         }
 
         protected override void OnStart()
