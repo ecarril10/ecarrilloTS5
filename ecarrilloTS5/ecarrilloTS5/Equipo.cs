1c86367aab2671b126e04f8fa2f5de273cb334d8
ELIAS <ELIAS@LAP-DELL>
2023-07-09 17:22:14 -0500

    Visualizar datos

---
+++ ecarrilloTS5/ecarrilloTS5/Equipo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ecarrilloTS5
+{
+    public class Equipo
+    {
+        public int codigo { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public string descripcion { get; set; }
+        public string observacion { get; set; }
+        public string estado { get; set; }
+
+
+    }
+}
--- ecarrilloTS5/ecarrilloTS5/MainPage.xaml.cs
+++ ecarrilloTS5/ecarrilloTS5/MainPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -10,9 +13,20 @@ namespace ecarrilloTS5
 {
     public partial class MainPage : ContentPage
     {
+        private String Url = "http://192.168.1.2/ws_qrCONNECTX/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<Equipo> post;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async Task btnConsultar_ClickedAsync(object sender, EventArgs e)
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<Equipo> listaPost = JsonConvert.DeserializeObject<List<Equipo>>(contenido);
+            post = new ObservableCollection<Equipo>(listaPost);
+            milista.ItemsSource = post;
         }
     }
 }
